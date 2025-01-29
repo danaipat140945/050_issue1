5ba5c3cbb7a82e0058f52f7b7378d073d3765a76
danaipat140945 <danaipat2002@gmail.com>
2025-01-29 09:34:27 +0700

    Update contact.php issues3

--- contact.php
+++ contact.php
@@ -9,14 +9,14 @@
             <td width="43" rowspan="2">&nbsp;</td>
         </tr>
         <tr>
-            <td width="1007" bgcolor="#FFFFFF">
+            <td width="1007" bgcolor="#FFFFFF">050
                 <img src="images/map.gif">
             </td>
             <td width="190" bgcolor="#FFFF66"><ul>
 				<li><a href="home.php">หน้าแรก</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่อ050</li>
+				<li><a href="contact.php">ติดต่อ</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
